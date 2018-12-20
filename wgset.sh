4b8cb0ecc40374bc6968e7945669dff8fc0468ee
350615774@qq.com <350615774@qq.com>
2018-12-20 15:39:45 +0800

    可一次增加多个文件

--- wgset.sh
+++ wgset.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 clear
 # 切换目录
+if [ ! -f "$sconf" ]; then
+mkdir /etc/wireguard
+fi
 cd /etc/wireguard
 
 # 生成key
@@ -39,6 +42,14 @@ cat << EOFF >> $sconf
 # firewall-cmd --reload
 
 [Interface]
+# 运行 WireGuard 时要执行的 iptables 防火墙规则，用于打开NAT转发之类的。
+# 如果你的服务器主网卡名称不是 eth0 ，那么请修改下面防火墙规则中最后的 eth0 为你的主网卡名称。
+#PostUp = iptables -A FORWARD -i ${sname} -j ACCEPT; iptables -A FORWARD -o ${sname} -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# 停止 WireGuard 时要执行的 iptables 防火墙规则，用于关闭NAT转发之类的。
+# 如果你的服务器主网卡名称不是 eth0 ，那么请修改下面防火墙规则中最后的 eth0 为你的主网卡名称。
+#PostDown = iptables -D FORWARD -i ${sname} -j ACCEPT; iptables -D FORWARD -o ${sname} -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE
+
 # ServerPriateKey
 PrivateKey = $privkey
 Address = ${svaddr}/24
@@ -52,23 +63,36 @@ function clien {
 clear
 cat $sconf
 read -e -p "Enter the client profile name: " cname
-cconf=${cname}.conf
-if [ -f "$cconf" ]; then
-echo "Profile $cconf already exists!!"
-exit
-fi
-read -e -p "Set the VPN client address[10.*.*.*]: " cvaddr
+#cconf=${cname}.conf
+#if [ -f "$cconf" ]; then
+#	echo "Profile $cconf already exists!!"
+#	exit
+#fi
+#read -e -p "Set the VPN client address[10.*.*.*]: " cvaddr
 ip a|grep inet|grep brd|awk '{print $1"\t"$2}'
 webip=`curl -s ip.6655.com/ip.aspx`
 echo "webip	$webip"
 read -e -p "Enter server address[serverIP]: " saddr
 read -e -p "Enter AllowedIPs[0.0.0.0/0]: " allowip
 
+caddra=`cat $sconf |grep Address | awk '{print $3}' | awk -F. '{print $1"."$2"."$3"."}'`
 spubkey=`cat $sconf | grep "# pubkey:" | awk '{print $3}'`
 sport=`cat $sconf | grep "ListenPort =" | awk '{print $3}'`
-mkkey
 
+ls
+read -e -p "Enter first number: " numa
+read -e -p "Enter last number: " numb
 # 写入客户端配置文件
+
+while [ ${numb} -ge ${numa} ]
+do
+cconf=${cname}${numa}.conf
+if [ -f "$cconf" ]
+then
+echo "Profile $cconf already exists!!"
+exit
+fi
+mkkey
 cat << EOFF > $cconf
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 # Client
@@ -76,7 +100,7 @@ cat << EOFF > $cconf
 # pubkey: $pubkey
 
 [Interface]
-Address = ${cvaddr}/24
+Address = ${caddra}.$[${numa}+1]/24
 # ClientPrivateKey
 PrivateKey = $privkey
 # Switch DNS server while connected
@@ -87,22 +111,25 @@ PrivateKey = $privkey
 PublicKey = $spubkey
 Endpoint = ${saddr}:$sport
 AllowedIPs = $allowip
-PersistentKeepalive = 25
+#PersistentKeepalive = 25
 
 EOFF
 
 # 给服务器增加[Peer]配置
 cat << EOFE >> $sconf
 [Peer]
-# $cname
+# $cconf
 # ClientPublicKey
 PublicKey = $pubkey
-AllowedIPs = ${cvaddr}/32
+AllowedIPs = ${caddra}.$[${numa}+1]/32
 
 EOFE
+
+numa=$[${numa}+1]
+done
+
 clear
 cat $sconf
-cat $cconf
 }
 
 # 菜单
