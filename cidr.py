fd38555c88385ed41b8c00ce4c93e4972a55f222
350615774@qq.com <29539539+nexply@users.noreply.github.com>
2019-01-04 15:50:06 +0800

    优化ip转化脚本

--- cidr.py
+++ cidr.py
@@ -10,10 +10,10 @@ nip = netaddr.IPAddress(sys.argv[2])
 ipf = [netaddr.IPAddress(ip.first), netaddr.IPAddress(nip.value + 1)]
 ipl = [netaddr.IPAddress(nip.value - 1), netaddr.IPAddress(ip.last)]
 
+print ""
 for i in range(2):
     startip = ipf[i]
     endip = ipl[i]
     cidrs = netaddr.iprange_to_cidrs(startip, endip)
-    for k, v in enumerate(cidrs):
-        iplist = v
+    for k, iplist in enumerate(cidrs):
         print "%s," % iplist,
