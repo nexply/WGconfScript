ec50cdc2feae745c719b01c020171eda22122a37
350615774@qq.com <350615774@qq.com>
2019-01-26 14:08:38 +0800

    修改ip段编辑脚本

--- cidr.py
+++ cidr.py
@@ -1,19 +1,29 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import sys
-import netaddr
+from netaddr import IPNetwork, IPAddress, IPSet
+from argparse import ArgumentParser
 
-ip = netaddr.IPNetwork(sys.argv[1])
-nip = netaddr.IPAddress(sys.argv[2])
 
-ipf = [netaddr.IPAddress(ip.first), netaddr.IPAddress(nip.value + 1)]
-ipl = [netaddr.IPAddress(nip.value - 1), netaddr.IPAddress(ip.last)]
+def main():
+    parser = ArgumentParser(description=u'需要输入“网段”和“IP”')
+    parser.add_argument('network', type=str, metavar=u'网段',
+                        help=u'输入网段，如：192.168.1.0/24')
+    parser.add_argument('ip', type=str, metavar='IP',
+                        help=u'输入要排除的ip，如：192.168.1.128')
+    args = parser.parse_args()
 
-print ""
-for i in range(2):
-    startip = ipf[i]
-    endip = ipl[i]
-    cidrs = netaddr.iprange_to_cidrs(startip, endip)
-    for k, iplist in enumerate(cidrs):
-        print "%s," % iplist,
+    ip = IPNetwork(args.network).cidr
+    nip = IPAddress(args.ip)
+
+    # ip = IPNetwork('192.168.1.23/16').cidr
+    # nip = IPAddress('192.168.1.123')
+
+    ips = IPSet([ip])
+    ips.remove(nip)
+
+    print ', '.join(outip for outip in map(str, ips.iter_cidrs()))
+
+
+if __name__ == '__main__':
+    main()
