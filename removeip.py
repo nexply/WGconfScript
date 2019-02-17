5717b98a9e1cb4f9edf8fe4a4be5eba3b451569d
350615774@qq.com <350615774@qq.com>
2019-02-17 21:31:38 +0800

    增加排除网段功能

--- removeip.py
+++ removeip.py
@@ -1,34 +1,28 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from netaddr import IPNetwork, IPAddress, IPSet
 from argparse import ArgumentParser
+
+from netaddr import IPNetwork, IPSet
 
 
 def main():
-    parser = ArgumentParser(description=u'支持从某个网段排除特定IP！')
-    parser.add_argument('network', type=str, metavar=u'网段',
-                        help=u'如：192.168.1.0/24')
-    parser.add_argument('ip', type=str, metavar=u'要排除的IP',
-                        help=u'如：192.168.1.128')
+    parser = ArgumentParser(description='支持从某个网段排除特定IP！')
+    parser.add_argument('network', type=str, metavar='网段',
+                        help='如：192.168.1.0/24')
+    parser.add_argument('ip', type=str, metavar='要排除的IP',
+                        help='如：192.168.1.128')
     args = parser.parse_args()
-
     try:
         ipnet = IPNetwork(args.network).cidr
+        ips = IPSet([ipnet])
     except Exception as e:
-        print u'输入网段错误：{}'.format(e)
-
+        print '输入网段错误：{}'.format(e)
     try:
-        nip = IPAddress(args.ip)
+        ips.remove(args.ip)
     except Exception as e:
-        print u'输入IP错误：{}'.format(e)
-
-    # ip = IPNetwork('192.168.1.23/16').cidr
-    # nip = IPAddress('192.168.1.123')
-
-    ips = IPSet([ipnet])
-    ips.remove(nip)
-
+        print '输入IP错误：{}'.format(e)
+    print '\n拆分后的IP段为：\n'
     print ', '.join(outip for outip in map(str, ips.iter_cidrs()))
 
 
